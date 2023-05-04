3d19fd396b85896e9b3c645a5a8bb56fa037c08b
Adam Leventhal <ahl@oxide.computer>
2023-05-04 14:43:43 -0700

    add predicates support under a feature flag (#17)

---
+++ src/feature_predicates.rs
@@ -0,0 +1,92 @@
+// Copyright 2023 Oxide Computer Company
+
+use std::{fmt::Display, path::PathBuf};
+
+use predicates::{reflection::PredicateReflection, Predicate};
+
+/// Creates a new predicate that ensures equality with the given file.
+///
+/// To accept changes to the file, run with `EXPECTORATE=overwrite`.
+#[cfg_attr(docsrs, doc(cfg(feature = "predicates")))]
+pub fn eq_file(path: impl Into<PathBuf>) -> FilePredicate {
+    let path = path.into();
+    FilePredicate { path, panic: false }
+}
+
+/// Creates a new predicate that ensures equality with the given file and
+/// panics if there's a mismatch.
+///
+/// To accept changes to the file, run with `EXPECTORATE=overwrite`.
+#[cfg_attr(docsrs, doc(cfg(feature = "predicates")))]
+pub fn eq_file_or_panic(path: impl Into<PathBuf>) -> FilePredicate {
+    let path = path.into();
+    FilePredicate { path, panic: true }
+}
+
+pub struct FilePredicate {
+    path: PathBuf,
+    panic: bool,
+}
+
+impl Display for FilePredicate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
+
+impl Predicate<str> for FilePredicate {
+    fn eval(&self, actual: &str) -> bool {
+        match crate::assert_contents_impl(&self.path, actual) {
+            Err(e) if self.panic => {
+                panic!("assertion failed: {e}")
+            }
+            Err(e) => {
+                println!("{e}");
+                false
+            }
+            Ok(_) => true,
+        }
+    }
+
+    fn find_case<'a>(
+        &'a self,
+        expected: bool,
+        variable: &str,
+    ) -> Option<predicates::reflection::Case<'a>> {
+        let actual = self.eval(variable);
+        if expected == actual {
+            Some(predicates::reflection::Case::new(None, actual))
+        } else {
+            None
+        }
+    }
+}
+
+impl PredicateReflection for FilePredicate {}
+
+#[cfg(test)]
+mod test {
+    use predicates::Predicate;
+
+    use crate::eq_file;
+
+    #[test]
+    fn predicates_good() {
+        let actual = include_str!("../tests/data_a.txt");
+        assert!(eq_file("tests/data_a.txt").eval(actual));
+    }
+
+    #[test]
+    #[should_panic]
+    fn predicates_bad() {
+        let actual = include_str!("../tests/data_a.txt");
+        assert!(eq_file("tests/data_b.txt").eval(actual));
+    }
+
+    #[test]
+    #[should_panic]
+    fn predicates_one_line_change() {
+        let actual = include_str!("../tests/data_a.txt");
+        assert!(eq_file("tests/data_a2.txt").eval(actual));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-// Copyright 2021 Oxide Computer Company
+// Copyright 2023 Oxide Computer Company
+
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 //! This library is for comparing multi-line output to data stored in version
 //! controlled files. It makes it easy to update the contents when should be
@@ -15,9 +17,9 @@
 //! If the output doesn't match, the program will panic! and emit the
 //! color-coded diffs.
 //!
-//! To accept the changes from `compose()`, run with ! `EXPECTORATE=overwrite`.
-//! Assuming `lyrics.txt` is checked in, `git diff` will show you something like
-//! this:
+//! To accept the changes from `compose()`, run with `EXPECTORATE=overwrite`.
+//! Assuming `lyrics.txt` is checked in, `git diff` will show you something
+//! like this:
 //!
 //! ```diff
 //! diff --git a/examples/lyrics.txt b/examples/lyrics.txt
@@ -32,39 +34,48 @@
 //! I'm especially good at expectorating
 //! -Ten points for Gaston
 //! ```
+//!
+//! # `predicates` feature
+//!
+//! Enable the `predicates` feature for compatibility with `predicates` via
+//! [`eq_file`] and [`eq_file_or_panic`].
+
+#[cfg(feature = "predicates")]
+mod feature_predicates;
+#[cfg(feature = "predicates")]
+pub use feature_predicates::*;
 
 use console::Style;
 use newline_converter::dos2unix;
 use similar::{Algorithm, ChangeTag, TextDiff};
-use std::{env, fs, path::Path};
+use std::{env, ffi::OsStr, fs, path::Path};
 
 /// Compare the contents of the file to the string provided
 #[track_caller]
 pub fn assert_contents<P: AsRef<Path>>(path: P, actual: &str) {
+    if let Err(e) = assert_contents_impl(path, actual) {
+        panic!("assertion failed: {e}")
+    }
+}
+
+pub(crate) fn assert_contents_impl<P: AsRef<Path>>(path: P, actual: &str) -> Result<(), String> {
     let path = path.as_ref();
     let var = env::var_os("EXPECTORATE");
-    let overwrite = match var.as_ref().map(|s| s.as_os_str().to_str()) {
-        Some(Some("overwrite")) => true,
-        _ => false,
-    };
+    let overwrite = var.as_deref().and_then(OsStr::to_str) == Some("overwrite");
 
     let actual = dos2unix(actual);
 
     if overwrite {
         if let Err(e) = fs::write(path, actual.as_ref()) {
-            panic!("unable to write to {}: {}", path.display(), e.to_string());
+            panic!("unable to write to {}: {}", path.display(), e);
         }
     } else {
-        // Treat non-existant files like an empty file.
+        // Treat a nonexistent file like an empty file.
         let expected_s = match fs::read_to_string(path) {
             Ok(s) => s,
             Err(e) => match e.kind() {
                 std::io::ErrorKind::NotFound => String::new(),
-                _ => panic!(
-                    "unable to read contents of {}: {}",
-                    path.display(),
-                    e.to_string()
-                ),
+                _ => panic!("unable to read contents of {}: {}", path.display(), e),
             },
         };
         let expected = dos2unix(&expected_s);
@@ -92,11 +103,12 @@ pub fn assert_contents<P: AsRef<Path>>(path: P, actual: &str) {
                 }
             }
             println!();
-            panic!(
-                r#"assertion failed: string doesn't match the contents of file: "{}" see diffset above
+            return Err(format!(
+                r#"string doesn't match the contents of file: "{}" see diffset above
                 set EXPECTORATE=overwrite if these changes are intentional"#,
                 path.display()
-            );
+            ));
         }
     }
+    Ok(())
 }
